666f222adeb4b8bde70aaad45c238dec10750960
qaz10102030 <qaz1010203044@gmail.com>
2019-11-16 15:03:19 +0800

    fix: Run app and add debug config

--- haoez_api_server/api.py
+++ haoez_api_server/api.py
@@ -3,8 +3,13 @@ from flask import Flask, jsonify
 from haoez_api_server import __version__
 
 app = Flask("haoez_api_server")
+app.config["DEBUG"] = True
 
 
 @app.route("/", methods=["GET"])
 def info():
     return jsonify({"version": __version__})
+
+
+if __name__ == "__main__":
+    app.run()
