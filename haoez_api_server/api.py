2eca04490d31c273645aff5e0a3bdcaa3d535d43
qaz10102030 <qaz1010203044@gmail.com>
2020-02-21 14:39:05 +0800

    feat: HSI檔案查詢入口完成

--- haoez_api_server/api.py
+++ haoez_api_server/api.py
@@ -1,11 +1,16 @@
+import datetime
+import json
+import os
 import sys
 
 import requests
-from flask import Flask, jsonify, request, send_from_directory
+from flask import Flask, abort, jsonify, request, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import select
 from sqlalchemy_utils import database_exists
 
 from __init__ import __version__
+from sql import table_v2 as table
 
 app = Flask("haoez_api_server")
 app.config["DEBUG"] = True
@@ -33,20 +38,14 @@ def test_db():
         return "<h1>Something is broken.</h1>"
 
 
-@app.route("/db/<table_name>")
-def db_table(table_name):
+@app.route("/hsi", methods=["GET"])
+def hsi_list():
     try:
-
-        from sql import table
-        import json
-
-        t = getattr(table, table_name)(db.metadata)
+        t = table.keylist(db.metadata)
         res = db.session.query(t).all()
         return jsonify(
             {
-                "desc": json.loads(
-                    str(t.c).replace(table_name + ".", "").replace("'", '"')
-                ),
+                "desc": json.loads(str(t.c).replace("keylist.", "").replace("'", '"')),
                 "result": res,
             }
         )
@@ -55,29 +54,54 @@ def db_table(table_name):
         return "<h1>Something is broken.</h1>"
 
 
-@app.route("/file", methods=["POST"])
-def file_post():
-    try:
-        # --debug--
-        # 檢查當下沒有data路徑 就用OS新建
-        import os
+@app.route("/hsi", methods=["POST"])
+def hsi_file_post():
 
+    try:
+        time_now = datetime.datetime.now()
+        time_now = time_now.strftime("%Y-%m-%d_%H_%M_%S")
+        f = request.files["file"]
+        camera_type = request.values["camera_type"]
+        sample_name = request.values["sample_name"]
+        res = f.filename
         if not os.path.isdir("./data/"):
             os.mkdir("./data/")
-        # --debug--
-        f = request.files["myfile"]
-        f.save("./data/" + f.filename)
-        return "ok!"
+
+        filename = camera_type + "_" + sample_name + "_" + time_now
+        f.save("./data/" + filename)
+
+        ins = (
+            table.keylist(db.metadata)
+            .insert()
+            .values(
+                Camera_name=camera_type,
+                File_name=res,
+                File_path=filename,
+                Sample_name=sample_name,
+            )
+        )
+        conn = db.engine.connect()
+        conn.execute(ins)
+        return "ok"
     except Exception as e:
         print(e, file=sys.stderr)
         return e
 
 
-@app.route("/file", methods=["GET"])
-def file_get():
-    filename = request.args.get("filename")
-    print(filename, file=sys.stderr)
-    return send_from_directory("../data/", filename)
+@app.route("/hsi/<id>", methods=["GET"])
+def hsi_file_get(id):
+    t = table.keylist(db.metadata)
+    res = select([t.c.File_path]).where(t.c.Keylist_id == id)
+    conn = db.engine.connect()
+    res = conn.execute(res).fetchone()
+
+    if res is None:
+        return abort(404)
+    else:
+        root_dir = os.path.dirname(os.getcwd())
+        return send_from_directory(
+            os.path.join(root_dir, "haoez_api_server", "data"), res[0]
+        )
 
 
 @app.route("/repository", methods=["POST"])
@@ -102,7 +126,6 @@ def demo():
         import demo
 
         img_path = demo.coffee(w, b, s, s_d)
-        # img_name = ""
         return jsonify({"result": "ok!", "url": img_path})
     except Exception as e:
         print("Outer")
